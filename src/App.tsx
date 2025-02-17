a7c443d58cb0d83d7849cf26141f27df3ddd9409
Sova42-cmd <steamsova42@gmail.com>
2025-02-17 09:10:16 +0400

    filtered odd numbers

--- src/App.tsx
+++ src/App.tsx
@@ -45,11 +45,16 @@ const fifthTasks = [
 
 let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 let evenNums = numbers.filter(isEven)
+let oddNums = numbers.filter(isOdd)
 
-console.log(evenNums)
+console.log(oddNums)
 
 function isEven(element:any){
     return element % 2 === 0;
+}
+
+function isOdd(element:any){
+    return element % 2 !== 0;
 }
 
 function App() {

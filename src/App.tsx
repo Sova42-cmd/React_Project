4246593d1a0c434f8815da7577b42ed7a4787242
Sova42-cmd <steamsova42@gmail.com>
2025-03-08 18:14:47 +0400

    Starting from scratch Pt.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,118 +1,46 @@
 import './App.css'
-import ToDoList from "./ToDoList.tsx";
-import Counter from "./Counter.tsx";
 import {v1} from "uuid";
+import ToDoList from "./ToDoList.tsx";
 import {useState} from "react";
 
-// In JavaScript, especially in React, code is often split into multiple files (or "modules") to keep things organized.
-// Instead of cramming all your components, functions, and logic into one giant file, you can:
+let firstTasks = [
+    {id: v1(), name: "Science", isDone: true},
+    {id: v1(), name: "History", isDone: false},
+    {id: v1(), name: "Mathematics", isDone: false},
+    {id: v1(), name: "Physical Education", isDone: true},
+    {id: v1(), name: "Subject", isDone: true},
+    {id: v1(), name: "Subject bul", isDone: false},
 
-// state, (set)functionChangingState(this function is newState as a parameter), parameter is new State. useState is Hook.
-// <"All"|"Active"|"Completed"> <-These are possible values or types of state
-// and () here's the initial state.
-//
-// Now let’s talk about the brackets ({}) in imports.
-//
-//     In React, there are two kinds of exports:
-//
-//     Default exports (like above) — you don’t need brackets.
-//     Named exports — you do need brackets.
-//
-// React relies on modular JavaScript because:
-//
-//     Components are reusable — You build small, independent pieces (like buttons, forms, etc.) that you can combine.
-//     Code splitting — Breaking files into smaller pieces helps React load parts of your app only when needed.
-//     Readability — Each file has a clear purpose.
-
-let firstTasks: any = [
-    {id: v1(), name: "Science", isDone: true,},
-    {id: v1(), name: "History", isDone: false,},
-    {id: v1(), name: "Art", isDone: true,},
-    {id: v1(), name: "Subject 4", isDone: true,},
-    {id: v1(), name: "Subject 5", isDone: false,},
-    {id: v1(), name: "Subject 6", isDone: false,},
-    {id: v1(), name: "Subject 7", isDone: true,},
-    {id: v1(), name: "Religion", isDone: true,},
 ]
-
-const buttons: any = [
-    {id: 1, name: "All"},
-    {id: 2, name: "Active"},
-    {id: 3, name: "Completed"}
-]
-
-// const secondTasks = [
-//     {id: 4, name: "Task1", isDone: true},
-//     {id: 5, name: "Task2", isDone: true},
-//     {id: 6, name: "Task3", isDone: false},
-// ]
-//
-// const thirdTasks = [
-//     {id: 7, name: "Task4", isDone: false},
-//     {id: 8, name: "Task5", isDone: true},
-//     {id: 9, name: "Task6", isDone: true},
-// ]
-//
-// const testButtons = [
-//     {id: 4, name: "Render New"},
-// ]
-//
-// const forthTasks = [
-//     {id: 10, name: "Task 7", isDone: true},
-//     {id: 11, name: "Task 8", isDone: true},
-//     {id: 12, name: "Task 9", isDone: true},
-// ]
-//
-// const fifthTasks = [
-//     {id: 13, name: "Task 10", isDone: false},
-//     {id: 14, name: "Task 11", isDone: true},
-//     {id: 15, name: "Task 12", isDone: false},
-// ]
 
 function App() {
 
-    let visual
+    let tasksToShow: any = firstTasks;
 
-    const [currentButton, setCurrentButton] = useState<"All"|"Active"|"Completed">("All")
-    const [tasksWithoutDelete, setTasksWithoutDelete] = useState(firstTasks)
+    const [selectedButton, setSelectedButton] = useState<"All" | "Active" | "Completed">("All")
 
-    function removeButton(messageID:any){
-        setTasksWithoutDelete(tasksWithoutDelete.filter((element:any)=>{return element.id !==messageID}))
+    function handleSelected(message: "All" | "Active" | "Completed") {
+        setSelectedButton(message)
     }
 
-    function handleButtonClick(message:any) {
-        setCurrentButton(message)
+    if (selectedButton === "All") {
+        tasksToShow = firstTasks
+    } else if (selectedButton === "Active") {
+        tasksToShow = firstTasks.filter((element) => {
+            return !element.isDone
+        })
+    } else if (selectedButton === "Completed") {
+        tasksToShow = firstTasks.filter((element) => {
+            return element.isDone
+        })
     }
 
-    function addTask(message:any) {
-        setTasksWithoutDelete(
-            [
-                {id: v1(), name: message, isDone: false}, ...tasksWithoutDelete
-            ]
-        )
-    }
 
-    if (currentButton === "All") {
-        visual=tasksWithoutDelete
-    } else if (currentButton === "Active") {
-        visual=tasksWithoutDelete.filter((element:any)=>{return !element.isDone})
-    } else if (currentButton === "Completed") {
-        visual=tasksWithoutDelete.filter((element:any)=>{return element.isDone})
-    }
-
-    console.log(visual)
 
     return (<>
-            <ToDoList title={"Necessarily Languages"} allTasks={visual} buttons={buttons}
-                      handleButtonClick={handleButtonClick}
-                      removeButton={removeButton}
-                      addTask={addTask}
-            />
-            {/*<ToDoList title={"Preferred to Know"} allTasks={filteredTasks} buttons={buttons}/>*/}
-            {/*<ToDoList title={"Optional"} allTasks={filteredTasks} buttons={buttons}/>*/}
-            {/*<ToDoList title={"Without a Title 1"} allTasks={filteredTasks} buttons={buttons}/>*/}
-            {/*<ToDoList title={"Without a Title 2"} allTasks={filteredTasks} buttons={testButtons}/>*/}
-            <Counter></Counter>
+
+            <ToDoList handleSelected={handleSelected} allTasks={tasksToShow}></ToDoList>
+
         </>
     )
 }
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,56 +1,26 @@
-import './App.css'
-import {useState} from "react";
-
 function ToDoList(props: any) {
 
-    const [inputValue, setInputValue] = useState('')
 
-    return (
-        <div className="app">
+    return (<>
             <div>
-                <h3>{props.title}</h3>
-                <div>
-                    <input
-                    value={inputValue}
-                    onChange={(event)=>{setInputValue(event.target.value)}}
-                    onKeyDown={(event)=>{
-                        if (event.key === 'Enter') {
-                            props.addTask(inputValue)
-                        }
-                    }}
-                    />
-                            <button onClick={()=>{props.addTask(inputValue)}}>+</button>
-                </div>
+                <input/><button>+</button>
                 <ul>
-                    {
-                        props.allTasks.length > 0 ?
-                            props.allTasks.map((element: any) => {
-                                return (
-                                    <li key={element.id}>
-                                        <input type="checkbox" checked={element.isDone}/>
-                                        <span>{element.name}</span>
-                                        <button
-                                            onClick={()=> {
-                                                props.removeButton(element.id)
-                                            }}
-                                        >x</button>
 
-                                    </li>
-                                )
-                            })
-                            : <li>"There are no tasks"</li>
-                    }
+                    {props.allTasks.map((element:any)=>{
+                        return<li>
+                            <input type='checkbox' checked={element.isDone}  />
+                            {element.name}
+                            <button>X</button>
+                        </li>
 
-                    <button onClick={()=>{props.handleButtonClick("All")}}>All</button>
-                    <button onClick={()=>{props.handleButtonClick("Active")}}>Active</button>
-                    <button onClick={()=>{props.handleButtonClick("Completed")}}>Completed</button>
+                    })}
+
                 </ul>
-                <div>
-
-                </div>
+                <button onClick={()=>{props.handleSelected("All")}}>All</button>
+                <button onClick={()=>{props.handleSelected("Active")}}>Active</button>
+                <button onClick={()=>{props.handleSelected("Completed")}}>Completed</button>
             </div>
-        </div>
-
+        </>
     )
 }
 

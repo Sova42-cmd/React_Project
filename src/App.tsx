35236452accb23e303773ec6e681a4d4c3d7fbf9
Aram Zakaryan <aram.zakaryan@n.rich>
2025-03-25 22:06:26 +0400

    destructuring lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -14,31 +14,21 @@ let initialTasks = [
 function App() {
 
 
-    // const tasks =[
-    //     {id: v1(), name: "Subject 1", isDone: false},
-    //     {id: v1(), name: "Subject 2", isDone: true}, // index=1
-    //     {id: v1(), name: "Subject 3", isDone: true},
-    //     {id: v1(), name: "Subject 4", isDone: false},
-    //     {id: v1(), name: "Subject 5", isDone: false},
-    //     {id: v1(), name: "Subject 6", isDone: true},
-    // ]
-
-
     // const person1 = {
     //     name: "Gago",
     //     age: 56,
     //     isMarried: true,
-    //     children: ['Valod', 'Styop', 'Slavik']
+    //     children: {
+    //         boys: ["Seroj", "Valod"],
+    //         girls: ["Lala", "Bella"],
+    //     }
     // }
+    //
+    // const updatedPerson = {         ...person1,   children: {...person1.children, boys:[...person1.children.boys,'gago j']}           }
+    //
+    // console.log(`updatedPerson`, updatedPerson)
 
-    // // mutable
-    // person1.children.push('Levonik');
 
-    // // immutable adding property (key and value)
-    // const updatedPerson1 = {...person1, parents: ['mama', 'papa']}
-
-    // // immutable updating property value
-    // const updatedPerson1 = {...person1, children:[...person1.children, 'Levonik']};
 
 
     let visibleTasks = initialTasks

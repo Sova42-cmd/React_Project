1149cef59c67528daa6a3fc40ae3140b96378f4d
Aram Zakaryan <aram.zakaryan@n.rich>
2025-03-25 21:02:13 +0400

    destructuring lesson

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,34 @@ let initialTasks = [
 
 function App() {
 
+
+    // const tasks =[
+    //     {id: v1(), name: "Subject 1", isDone: false},
+    //     {id: v1(), name: "Subject 2", isDone: true}, // index=1
+    //     {id: v1(), name: "Subject 3", isDone: true},
+    //     {id: v1(), name: "Subject 4", isDone: false},
+    //     {id: v1(), name: "Subject 5", isDone: false},
+    //     {id: v1(), name: "Subject 6", isDone: true},
+    // ]
+
+
+    // const person1 = {
+    //     name: "Gago",
+    //     age: 56,
+    //     isMarried: true,
+    //     children: ['Valod', 'Styop', 'Slavik']
+    // }
+
+    // // mutable
+    // person1.children.push('Levonik');
+
+    // // immutable adding property (key and value)
+    // const updatedPerson1 = {...person1, parents: ['mama', 'papa']}
+
+    // // immutable updating property value
+    // const updatedPerson1 = {...person1, children:[...person1.children, 'Levonik']};
+
+
     let visibleTasks = initialTasks
 
     const [buttonsBelow, setButtonsBelow] = useState<"All" | "Active" | "Completed">("All");
@@ -28,7 +56,8 @@ function App() {
             return element.id !== taskID
         }))
     }
-    function addTask(sourceInput:string) {
+
+    function addTask(sourceInput: string) {
         setVisibleTasksWithDelete(
             [
                 {id: v1(), name: sourceInput, isDone: false}, ...visibleTasksWithDelete
